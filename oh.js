50c1a82d33be225e19e9257e36772d01edd9cc82
Samikshya Baniya Chhetri <118588912+SamikshyaChhetri@users.noreply.github.com>
2024-05-04 18:30:12 +0545

    added new image

---
+++ oh.js
@@ -0,0 +1,7 @@
+const element=document.getElementById('hello')
+element.addEventListener('click',function(){
+    // disp.innerHTML+='<h2>HEHE....</h2>';
+    const a=document.getElementById('disp')
+    a.innerHTML+='<h2>HEHE....</h2>';
+
+})
--- script.js
+++ script.js
@@ -1,27 +1,18 @@
-// const element=document.getElementById('ip');
-// const myelement=element.value;
-// console.log(myelement);
-// setTimeout(function(){
-//     const element=document.getElementById('ip');
-// const myelement=element.value;
-// console.log(myelement);
-// },5000)
-let tries=0;
-const sbc=Math.floor(Math.random()*10);
-const ele=document.getElementById('check');
-ele.addEventListener('click', function(){
-    tries++;
-    const element=document.getElementById('ip');
-    const myelement=element.value;
-    const disp=document.getElementById('display');
-    if(sbc==myelement){
-        disp.innerHTML='You guessed it correct. Tries: '+tries;
-    }
-    else if(sbc>myelement){
-        disp.innerHTML='You guessed it wrong.Your guess is lower. Tries: '+tries;
-    }
-    else if(sbc<myelement){
-        disp.innerHTML='You guessed it wrong.Your guess is higher. Tries: '+tries;
-    }
+let tries = 0;
+const sbc = Math.floor(Math.random() * 10);
+const ele = document.getElementById("check");
+ele.addEventListener("click", function () {
+  tries++;
+  const element = document.getElementById("ip");
+  const myelement = element.value;
+  const disp = document.getElementById("display");
+  if (sbc == myelement) {
+    disp.innerHTML = "You guessed it correct. Tries: " + tries;
+  } else if (sbc > myelement) {
+    disp.innerHTML =
+      "You guessed it wrong.Your guess is lower. Tries: " + tries;
+  } else if (sbc < myelement) {
+    disp.innerHTML =
+      "You guessed it wrong.Your guess is higher. Tries: " + tries;
+  }
 });
-

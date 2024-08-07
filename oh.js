547bdf31b45f4edd5580f85a96d600dafd672a00
Samikshya Baniya Chhetri <118588912+SamikshyaChhetri@users.noreply.github.com>
2024-08-07 10:31:00 +0545

    Random number guesser

--- oh.js
+++
@@ -1,7 +0,0 @@
-const element=document.getElementById('hello')
-element.addEventListener('click',function(){
-    // disp.innerHTML+='<h2>HEHE....</h2>';
-    const a=document.getElementById('disp')
-    a.innerHTML+='<h2>HEHE....</h2>';
-
-})
